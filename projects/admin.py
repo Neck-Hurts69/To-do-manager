93d50cea7d665a354ffb8327f4b98c550f64d78a
JolyTh3Kidd <jolythekidd@gmail.com>
2026-01-04 14:30:12 +0500

    Added filter, search, etc.

--- projects/admin.py
+++ projects/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from .models import Team
-from .models import Task
-from .models import Project
+from django.utils.html import format_html
+from .models import Task, Team, Project
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'team',
+        'responsible',
+        'status',
+        'due_date',
+        'overdue_status',
+    )
+    list_filter = ('status','team','due_date',)
+    search_fields = ('title','description',)
 
 admin.site.register(Team)
 admin.site.register(Task)
